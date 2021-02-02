345c42b01a41268d111d024b346cb71e4f09d5ce
hashemipeyman <hashemipeyman@users.noreply.github.com>
2021-02-02 16:37:12 +0330

    Add postgres connection to settings.py

--- geosmartsuite/settings.py
+++ geosmartsuite/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'geosmartsuite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geodb',
+        'USER': 'map',
+        'PASSWORD': 'miniso10',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
