ae1e2e106b38ea6279e7896ceeb5551970051a7f
hashemipeyman <hashemipeyman@users.noreply.github.com>
2021-02-02 13:52:13 +0330

    Debug

--- geosmartsuite/urls.py
+++ geosmartsuite/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
+from django.urls import include, path
+
 urlpatterns = [
     path('geoapp_test/', include('geoapp_test.urls')),
     path('admin/', admin.site.urls),
