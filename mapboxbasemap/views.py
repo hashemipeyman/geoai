fc48668d7d26fe8fbe7b1f08559ee2244b3a6623
hashemipeyman <hashemipeyman@users.noreply.github.com>
2021-02-02 17:14:49 +0330

    Add mapbox access token

--- mapboxbasemap/views.py
+++ mapboxbasemap/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def default_map(request):
-    return render(request, 'default.html', {})
+    # TODO: move this token to Django settings from an environment variable
+    # found in the Mapbox account settings and getting started instructions
+    # see https://www.mapbox.com/account/ under the "Access tokens" section
+    mapbox_access_token = 'pk.eyJ1IjoiaGFzaGVtaXBleW1hbiIsImEiOiJja2tvMW8xMmYxenJqMnZ0ZHEwcm9uZW9uIn0.pUtfPgFF3IkJ2L8-CIuMhw'
+    return render(request, 'default.html',
+                  { 'mapbox_access_token': mapbox_access_token })
